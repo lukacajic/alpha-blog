d5ab243a0c7af629ef95ee07ce7ac01e9f05777c
Alpha Blog <lukacajic@gmail.com>
2019-06-03 19:41:33 +0000

    Extract redundandcy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -7,44 +9,44 @@ class ArticlesController < ApplicationController
         @article = Article.new
   end
     
-  def edit 
-       @article = Article.find(params[:id]) 
+  def edit
   end
     
   def create
-      @article = Article.new(article_params)
+    @article = Article.new(article_params)
       if @article.save
-          flash[:notice] = "Article was successfully created"
-          redirect_to article_path(@article)
+        flash[:notice] = "Article was successfully created"
+        redirect_to article_path(@article)
       else
-         render 'new' 
+        render 'new' 
       end
   end
   
   def update
-     @article = Article.find(params[:id])
-     if @article.update(article_params)
-       flash[:notice] = "Article was sucessfully updated"
-       redirect_to article_path(@article)
-     else
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
        render 'edit'
-     end
+    end
   end
   
   def show
-     @article = Article.find(params[:id]) 
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
-      def article_params
-          params.require(:article).permit(:title, :description)
-      end
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
   
 end
